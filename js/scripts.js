8f0b617d46b0a3c46302bc1684bd59b84bf1ce73
cg <chasepgrijalva@gmail.com>
2017-04-28 10:03:20 -0700

    added branching and icons for each language

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
-  $("form#option").submit(function(event) {
+  $("form#questions").submit(function(event) {
   var ques1 = $("input:radio[name=ques1]:checked").val();
   var ques2 = $("input:radio[name=ques2]:checked").val();
   var ques3 = $("input:radio[name=ques3]:checked").val();
 
 
-  if (ques1 === "c#php" && ques2 === "coupe" && ques3 === "green") {
-    console.log(make, color);
-    $("#newcar").hide();
-    $("#option").hide();
-    $(".bmwcoupegrn").show();
+  if (ques1 === "c#php" && ques2 === "c#" && ques3 === "c#") {
+    console.log(ques1, ques2, ques3);
+    $("#track").hide();
+    $("#questions").hide();
+    $("#try").show();
+    $(".c#").show();
   }
 
-  else if (make === "bmw" && model === "coupe" && color === "red") {
-    console.log(make, color);
-    $("#newcar").hide();
-    $("#option").hide();
-    $(".bmwcoupered").show();
+  else if (ques1 === "c#php" && ques2 === "php" && ques3 === "php") {
+    console.log(ques1, ques2, ques3);
+    $("#track").hide();
+    $("#questions").hide();
+    $("#try").show();
+    $(".php").show();
   }
+  event.preventDefault();
+  });
+});
